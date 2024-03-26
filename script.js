9bcb0498a519d2eccf4895b98f1aaf996f6a366a
Shayon Maramkhah <frost00@github.com>
2024-03-26 13:08:26 -0400

    Adding more files

---
+++ script.js
@@ -0,0 +1,10 @@
+function DesktopAlert(){
+    alert("Serving as a Field Service Technician for Kubtec Medical Imaging has taught me to keep track of quality control issues in Excel. In addition I have experience troubleshooting software applications using simple commands such as ping, ipconfig and more. Software such as event viewer allowed me to recognize the reasons for any crashes and restore point was used to create backups of the computers state.")
+}
+
+function NetworkTech(){
+    alert("Serving as a Network Technician at Western Connecticut State Univeristy has increased my knowledge of onsite computer networks. I have performed some cabling in IDF closets exposing me to routers and switches. This kepted me up to date on naming conventions and infrastructure standards. In addition I have fixed wireless data access points and completed student and staff work orders.")
+}
+function SelfStudy(){
+    alert("Combining these experiences has driven me to self study topics such as cloud computing. Amazon Web Services serverless architecture, Lambda. Microsoft Azure and other cloud computing software.")
+}
